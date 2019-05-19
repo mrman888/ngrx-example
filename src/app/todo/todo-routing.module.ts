921cdbb1f645a148b15c9f1e14b3ab092ce16a05
mrman888 <khdmdcm@gmail.com>
2019-05-19 14:36:23 +0200

    add

--- src/app/todo/todo-routing.module.ts
+++ src/app/todo/todo-routing.module.ts
@@ -9,4 +9,4 @@ const routes: Routes = [{ path: '', component: TodoComponent }];
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class TodoRoutingModule {}
+export class TodoRoutingModule { }
